9846b4d3c313542b5d93954d2e16332be6cab904
Akshay Arora <akshayarora.10.6.97@gmail.com>
2020-08-15 21:42:19 +0530

    Added sample project

--- app/src/main/java/com/luciferx86/doodlecanvas/MainActivity.kt
+++ app/src/main/java/com/luciferx86/doodlecanvas/MainActivity.kt
@@ -1,15 +1,72 @@
 package com.luciferx86.doodlecanvas
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
 import com.luciferx86.doodlecanvaslibrary.DoodleCanvas
 
 class MainActivity : AppCompatActivity() {
+    lateinit var redButton: View;
+    lateinit var greenButton: View;
+    lateinit var yellowButton: View;
+    lateinit var pinkButton: View;
+    lateinit var blueButton: View;
+    lateinit var slider: SeekBar;
+    lateinit var canvas: DoodleCanvas;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val canvas: DoodleCanvas = findViewById(R.id.doodleCanvas);
-        canvas.setStrokeColor(Color.parseColor("#00ffbf"));
+        initViews();
+
+        setClickListeners();
+    }
+
+    private fun initViews(){
+        canvas = findViewById(R.id.doodleCanvas);
+        redButton = findViewById(R.id.redButton);
+        yellowButton = findViewById(R.id.yellowButton);
+        greenButton = findViewById(R.id.greenButton);
+        blueButton = findViewById(R.id.blueButton);
+        pinkButton = findViewById(R.id.pinkButton);
+        slider = findViewById(R.id.slider);
+    }
+
+    private fun setClickListeners(){
+        redButton.setOnClickListener{
+            canvas.setStrokeColor(Color.parseColor("#ff0000"));
+        }
+
+        blueButton.setOnClickListener{
+            canvas.setStrokeColor(Color.parseColor("#00c8ff"));
+        }
+
+        yellowButton.setOnClickListener{
+            canvas.setStrokeColor(Color.parseColor("#ffff00"));
+        }
+
+        greenButton.setOnClickListener{
+            canvas.setStrokeColor(Color.parseColor("#00ff99"));
+        }
+
+        pinkButton.setOnClickListener{
+            canvas.setStrokeColor(Color.parseColor("#f5b0e5"));
+        }
+
+        slider.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                canvas.setStrokeWidth(progress.toFloat());
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 }
